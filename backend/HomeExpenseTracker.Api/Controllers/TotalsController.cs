c927abdcf793e1c5e3c948df144fe4aaf69654d2
branquelao <henriquetozzonarciso@gmail.com>
2026-07-05 15:11:18 -0300

    feat: add totals endpoint and register service

---
+++ backend/HomeExpenseTracker.Api/Controllers/TotalsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using HomeExpenseTracker.Api.Services;
+
+namespace HomeExpenseTracker.Api.Controllers
+{
+    [ApiController]
+    [Route("api/totals")]
+    public class TotalsController : ControllerBase
+    {
+        private readonly ITotalsService _totalsService;
+
+        public TotalsController(ITotalsService totalsService)
+        {
+            _totalsService = totalsService;
+        }
+
+        /// <summary>Returns income/expense/balance totals per person and overall.</summary>
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(await _totalsService.GetTotalsAsync());
+        }
+    }
+}
